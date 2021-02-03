56331cdb86227a9f64bf094742fa44a6e4f35878
thusharathiwanka <thusharathiwanka123@gmail.com>
2021-02-03 20:06:21 +0530

    CREATE: mobile nav functionality added

---
+++ app/js/script.js
@@ -0,0 +1,18 @@
+const navOpen = document.querySelector(".menu-open");
+const navClose = document.querySelector(".menu-close");
+const navLinks = document.querySelector("nav ul");
+const logo = document.querySelector(".logo");
+
+navOpen.addEventListener("click", () => {
+  navClose.style.display = "block";
+  navClose.style.pointerEvents = "all";
+  navClose.style.position = "fixed";
+  navLinks.style.display = "flex";
+  logo.style.position = "fixed";
+});
+
+navClose.addEventListener("click", () => {
+  navOpen.style.display = "block";
+  navClose.style.display = "none";
+  navLinks.style.display = "none";
+});
